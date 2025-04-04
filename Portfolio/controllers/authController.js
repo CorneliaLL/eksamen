5d79416380211fe5407949a92d587205b6c12b94
celinaaee <ceel24ab@student.cbs.dk>
2025-04-04 16:43:23 +0200

    sign up log in

--- Portfolio/controllers/authController.js
+++ Portfolio/controllers/authController.js
@@ -42,42 +42,49 @@ class AuthController{
 }
     */
 
-// SIGNUP controller – calls our createUser and sends respons to client
+// SIGNUP controller – handles user registration and stores the new user in our DB
 async function signup (req, res){
     try {
       const { name, username, email, password, age } = req.body;
-  
+
+  //Signup validation - username must be at least 3 characters
       if (!username || username.length < 3) {
         return res.status(400).json({ error: "Username must be at least 3 characters" });
       }
   
-      // Kalder modellen der gemmer i databasen
+      // Variable that calls the createUser function from userModels to save the user in our DB
       const newUser = await createUser({ name, username, email, password, age });
   
+      //Sends us a response that the sign up is a success
       res.status(201).json({
         message: "User created successfully",
         user: newUser,
       });
   
+      //Catch error that sends server error respons if sign up is a fail
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
   };
 
-  // LOGIN controller – checks credentials
+  // LOGIN controller – checks if the log in is valid and verifies the users access
 async function login(req, res) {
     try {
       const { email, password } = req.body;
   
+    // Calls our findUserByEmail function from userModels to check if the user exists in our DB or not
       const user = await findUserByEmail(email);
+
+    //Checks if the user exists
       if (!user) {
         return res.status(404).json({ error: "User not found" });
       }
-  
-      if (user.password !== password) {
+      //Checks if the password is correct
+      else if (user.password !== password) {
         return res.status(401).json({ error: "Incorrect password" });
-      }
-  
+      } else {
+    
+    //If everything is correct, returns a success response
       res.status(200).json({
         message: "Login successful",
         user: {
@@ -86,7 +93,9 @@ async function login(req, res) {
           email: user.email
         }
       });
+    }
   
+    //Catch error, handles errors in the verification
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
