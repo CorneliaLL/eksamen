7391bab055f6b6b435be16723197f0ce45d89269
celinaaee <ceel24ab@student.cbs.dk>
2025-04-06 15:11:19 +0200

    n

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const app = express()
-
 //middleware package
 const cors = require("cors") 
-app.use(express.static("public")); //makes content in public visible and accessible in the browser 
-app.set("view engine", "ejs"); //makes it possible to show dynamic html-pages in ejs 
-app.use(express.urlencoded({ extended: true })); //makes it possible to read data from html-formulares
+
 app.use(cors()) //without we get errors when we get request to the endpoints 
 app.use(express.json()) //without this we coukd not return json like we do in the /users  endpoint 
+app.use(express.urlencoded({ extended: true })); //makes it possible to read data from html-formulares
+app.use(express.static("public")); //makes content in public visible and accessible in the browser 
+app.set("view engine", "ejs"); //makes it possible to show dynamic html-pages in ejs 
+
 
 //Routes
 const authRoutes = require('./routes/authRoute.js');
 const dashboardRoutes = require('./routes/dashboardRoute.js')
+const accountRoutes = require('./routes/accountRoute.js')
 
+
+//
 app.use('/auth', authRoutes);
 app.use('/dashboard', dashboardRoutes)
+app.use('/account', accountRoutes)
 
+//Standard routes
 //endpoint = URL
 //This is a API endpoint bc it returns data and not a html-page 
 app.get("/data", (req, res) => {
@@ -27,29 +33,6 @@ app.get("/", (req, res) => {
     res.render("index", { msg: "Welcome" });
 })
 
-//Outcommented for now
-//login post request (fetch req in frontend )
-/*
-app.get('/users', userController.getAllUsers)
-
- //{ id: 1, name: "cornelia", }
-app.get('/users/1', userController.getSingleUser)
-    
-app.post('/login', authController.login)
-
-app.post('/logout', authController.logout)
-
-app.post('/signup', authController.signup)
-
-app.post('/createAccount', authController.createAccount)
-
-//app.get('/viewAccount', userController.viewAccount)
-
-app.post('/withdraw', accountController.withdrawAmount)
-
-*/
-
-//app.get('/dashboard', dashboardController.renderDashboard)
 
 
 
