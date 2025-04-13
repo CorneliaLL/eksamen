bf69b8c5c3773b96e3eefd9fe88d97106ac60f9a
celinaaee <ceel24ab@student.cbs.dk>
2025-04-13 13:14:50 +0200

    sql

--- db.sql
+++ db.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Users (
   userID INT PRIMARY KEY IDENTITY(1,1),
   name NVARCHAR(100) NOT NULL,
-  username NVARCHAR(100),
-  email NVARCHAR(100),
-  password NVARCHAR(100),
+  username NVARCHAR(100) NOT NULL,
+  email NVARCHAR(100) NOT NULL,
+  password NVARCHAR(100) NOT NULL,
   age INT NOT NULL
 );
 
@@ -15,11 +15,11 @@ bankName NVARCHAR (100) NOT NULL
 CREATE TABLE Accounts (
   accountID INT PRIMARY KEY IDENTITY(1,1),
   userID INT NOT NULL,
-  currency VARCHAR(50),
-  balance DECIMAL(18,2),
-  registrationDate DATETIME,
-  accountStatus BIT,
-  bankID INT,
+  currency VARCHAR(50) NOT NULL,
+  balance DECIMAL(18,2) NOT NULL,
+  registrationDate DATETIME NOT NULL,
+  accountStatus BIT NOT NULL,
+  bankID INT NOT NULL,
 
   FOREIGN KEY (userID) REFERENCES Users(userID),
   FOREIGN KEY (bankID) REFERENCES Banks(bankID)
@@ -32,4 +32,48 @@ CREATE TABLE Portfolios (
   registrationDate DATETIME NOT NULL,
 
   FOREIGN KEY (accountID) REFERENCES Accounts(accountID)
+);
+
+CREATE TABLE Stocks (
+  stockID INT PRIMARY KEY IDENTITY(1,1),
+  portfolioID INT NOT NULL,
+  stockName NVARCHAR(100) NOT NULL,
+  currentPrice FLOAT NOT NULL,
+  stockType NVARCHAR(100) NOT NULL,
+  currency FLOAT NOT NULL
+  FOREIGN KEY (portfolioID) REFERENCES [dbo].[Portfolios](stockID)
+);
+
+CREATE TABLE Pricehistory (
+  historyID INT PRIMARY KEY IDENTITY(1,1),
+  stockID INT NOT NULL,
+  price DECIMAL (18,4) NOT NULL,
+  priceDate DATETIME2 NOT NULL,
+  FOREIGN KEY (stockID) REFERENCES [dbo].[Stocks](stockID)
+);
+
+CREATE TABLE Trades (
+    tradeID INT PRIMARY KEY,
+    portfolioID INT NOT NULL,
+    stockID INT NOT NULL,
+    tradeType VARCHAR(4) CHECK (tradeType IN ('buy', 'sell')) NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(18,4) NOT NULL,
+    fee DECIMAL(18,4) NOT NULL,
+    totalPrice DECIMAL(18,4) NOT NULL,
+    date DATETIME NOT NULL,
+
+    FOREIGN KEY (portfolioID) REFERENCES Portfolios(portfolioID),
+    FOREIGN KEY (stockID) REFERENCES Stocks(stockID)
+);
+
+CREATE TABLE Transactions (
+    transactionID INT PRIMARY KEY,
+    tradeID INT NOT NULL,
+    accountID INT NOT NULL,
+    amount DECIMAL(18,4) NOT NULL,
+    date DATETIME NOT NULL,
+
+    FOREIGN KEY (tradeID) REFERENCES Trades(tradeID),
+    FOREIGN KEY (accountID) REFERENCES Accounts(accountID)
 );
