67465eab679d64919532916929d20e3a6cf31798
celinaaee <ceel24ab@student.cbs.dk>
2025-04-13 13:40:31 +0200

    c

--- Portfolio/controllers/accountController.js
+++ Portfolio/controllers/accountController.js
@@ -1,17 +1,4 @@
-class AccountController{
 
-    withdrawAmount(req, res){
-        const { currency, amount } = req.body
-
-        res.send("3")
-
-        console.log(currency, amount)
-    }
-}
-
-function createAccount(){
-
-}
 
 module.exports = { 
     AccountController,
