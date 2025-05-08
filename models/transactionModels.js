fb8ff142f6f03818ee4051455d5a2489c0129765
Edanurk00 <edanurk@icloud.com>
2025-05-08 09:41:41 +0200

    k

--- models/transactionModels.js
+++ models/transactionModels.js
@@ -1,3 +1,5 @@
+const { connectToDB, sql } = require("../database");
+
 class Transaction {
   constructor(transactionID, accountID, tradeID, amount, transactionDate = new Date()) {
     this.transactionID = transactionID;
@@ -7,30 +9,66 @@ class Transaction {
     this.transactionDate = transactionDate;
   }
 
-  async registerTransaction() {
-    try {
-      const pool = await connectToDB();
-      await pool.request()
-        .input("accountID", sql.Int, this.accountID)
-        .input("tradeID", sql.Int, this.tradeID)
-        .input("amount", sql.Decimal(18, 2), this.amount)
-        .input("transactionDate", sql.DateTime, this.transactionDate)
-        .query(`
-          INSERT INTO Transactions (accountID, tradeID, amount, transactionDate)
-          VALUES (@accountID, @tradeID, @amount, @transactionDate)
-        `);
-
-      await pool.request()
-        .input("accountID", sql.Int, this.accountID)
-        .input("amount", sql.Decimal(18, 2), this.amount)
-        .query(`
-          UPDATE Accounts
-          SET balance = balance + @amount
-          WHERE accountID = @accountID
-        `);
-    } catch (err) {
-      console.error("Error in registerTransaction (object):", err.message);
-      throw err;
+    // Registrer en ny transaction i databasen og opdater kontobalancen 
+    static async registerTransaction({ accountID, tradeID, amount, transactionDate = new Date() }) {
+      try {
+        const pool = await connectToDB();
+  
+        // Indsætter en ny række i Transactions-tabellen
+        await pool.request()
+          .input("accountID", sql.Int, accountID)
+          .input("tradeID", sql.Int, tradeID)
+          .input("amount", sql.Decimal(18, 2), amount)
+          .input("transactionDate", sql.DateTime, transactionDate)
+          .query(`
+            INSERT INTO Transactions (accountID, tradeID, amount, transactionDate)  
+            VALUES (@accountID, @tradeID, @amount, @transactionDate)
+          `);
+  
+        // Opdaterer kontobalancen i Accounts-tabellen
+        await pool.request()
+          .input("accountID", sql.Int, accountID)
+          .input("amount", sql.Decimal(18, 2), amount)
+          .query(`
+            UPDATE Accounts
+            SET balance = balance + @amount
+            WHERE accountID = @accountID
+          `);
+      } catch (err) {
+        console.error("Error in registerTransaction:", err.message);
+        throw err; 
+      }
+    }
+  
+    // Henter alle transaktioner for en given konto
+    static async getTransactions(accountID) {
+      try {
+        const pool = await connectToDB();
+  
+        const result = await pool.request()
+          .input("accountID", sql.Int, accountID)
+          .query(`
+            SELECT 
+              Transactions.transactionID,  
+              Transactions.accountID,
+              Transactions.tradeID,
+              Transactions.amount,
+              Transactions.transactionDate,
+              Trades.Ticker, 
+              Trades.tradeType
+            FROM Transactions
+            LEFT JOIN Trades ON Transactions.tradeID = Trades.tradeID -- LEFT JOIN får alle transaktioner, både trades og almindelige
+            WHERE Transactions.accountID = @accountID
+            ORDER BY Transactions.transactionDate DESC -- Sorterer så nyeste transaktioner kommer først
+          `);
+  
+        return result.recordset;
+      } catch (err) {
+        console.error("Error in getTransactions:", err.message);
+        throw err; 
+      }
     }
   }
-}
+  
+  module.exports = { Transaction };
+
