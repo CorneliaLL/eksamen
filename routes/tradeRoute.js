d84d9f113313b5baca3d4563a829d0d60192fa03
celinaaee <ceel24ab@student.cbs.dk>
2025-05-11 17:55:09 +0200

    444

--- routes/tradeRoute.js
+++ routes/tradeRoute.js
@@ -4,12 +4,12 @@ const { handleTrade } = require("../controllers/tradeController");
 const { handleStockSearch } = require("../controllers/stockController");
 
 
-router.get('/trade/:portfolioID/:accountID', handleTrade)
-router.get("/trade/:portfolioID/:accountID/stocksearch", handleStockSearch);
+router.get('/trade', handleTrade)
+router.get("/trade/:portfolioID/:accountID", handleStockSearch);
 
 
 router.post("/search", handleStockSearch);
-router.post("/trade/:portfolioID/:accountID/stocksearch", handleStockSearch);
-router.post("/trade/:portfolioID/:accountID", handleTrade);
+router.post("/trade/:portfolioID/:accountID", handleStockSearch);
+router.post("/trade", handleTrade);
 
 module.exports = router;
