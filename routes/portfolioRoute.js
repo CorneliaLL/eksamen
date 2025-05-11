0187e5c87c4e22444746967f31750638b0716f0b
celinaaee <ceel24ab@student.cbs.dk>
2025-05-11 16:34:16 +0200

    m

--- routes/portfolioRoute.js
+++ routes/portfolioRoute.js
@@ -7,7 +7,7 @@ router.get('/portfolio/:portfolioID', portfolioController.handleCreatePortfolio)
 router.get('/createPortfolio', portfolioController.renderCreatePortfolio);
 router.get("/portfolio", portfolioController.getPortfolios, accountController.getAccountByID);
 router.get("/portfolio/:portfolioID/:accountID", portfolioController.getPortfolioByID);
-router.get('/api/portfolio/:portfolioID/graph', portfolioController.getPortfolioGraphData);
+router.get("/api/portfolio/:portfolioID/graph", portfolioController.getPortfolioGraphData);
 router.get("/stock-chart/:ticker", (req, res) => {
     res.render('stockChart', { ticker: req.params.ticker });
 });
